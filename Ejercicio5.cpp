893ab01e9d031668608e01fb112a2c353c2a7d0c
codegunner187 <laboratorio.simulacro187@gmail.com>
2026-09-17 09:18:20 -0400

    Create Ejercicio5.cpp

---
+++ Ejercicio5.cpp
@@ -0,0 +1,6 @@
+// Copiar el ejercicio 5 debajo del comentario "{ EJERCICIO 5 COPIAR ABAJO }"
+
+
+
+//==========================================================================================
+// Copiar la llamada del ejercicio 5 debajo del comentario "{ EJERCICIO 5 COPIAR ABAJO }"
